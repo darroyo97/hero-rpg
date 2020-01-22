42bdffc4781aebc1f92e9d03de7ab3a1ad49ea70
Daniela <daniela.arroyo27@gmail.com>
2020-01-22 12:55:58 -0600

    changed classes into one class called characters

--- hero_rpg.py
+++ hero_rpg.py
@@ -23,16 +23,20 @@ class Goblin(Character):
         super(Goblin, self).__init__(6, 2)
 
 
+hero = Hero()
+goblin = Goblin()
+
+
 def main():
     # hero_health = 10
     # hero_power = 5
     # goblin_health = 6
     # goblin_power = 2
 
-    while goblin_health > 0 and hero_health > 0:
-        print("You have {} health and {} power.".format(hero_health, hero_power))
+    while goblin.health > 0 and hero.health > 0:
+        print("You have {} health and {} power.".format(hero.health, hero.power))
         print("The goblin has {} health and {} power.".format(
-            goblin_health, goblin_power))
+            goblin.health, goblin.power))
         print()
         print("What do you want to do?")
         print("1. fight goblin")
@@ -42,9 +46,9 @@ def main():
         raw_input = input()
         if raw_input == "1":
             # Hero attacks goblin
-            goblin_health -= hero_power
-            print("You do {} damage to the goblin.".format(hero_power))
-            if goblin_health <= 0:
+            goblin.health -= hero.power
+            print("You do {} damage to the goblin.".format(hero.power))
+            if goblin.health <= 0:
                 print("The goblin is dead.")
         elif raw_input == "2":
             pass
@@ -54,11 +58,11 @@ def main():
         else:
             print("Invalid input {}".format(raw_input))
 
-        if goblin_health > 0:
+        if goblin.health > 0:
             # Goblin attacks hero
-            hero_health -= goblin_power
-            print("The goblin does {} damage to you.".format(goblin_power))
-            if hero_health <= 0:
+            hero.health -= goblin.power
+            print("The goblin does {} damage to you.".format(goblin.power))
+            if hero.health <= 0:
                 print("You are dead.")
 
 
