45f5b6b835d6021bc24b7324a0a3b8116c031463
Daniela <daniela.arroyo27@gmail.com>
2020-01-27 00:26:08 -0600

    refactor while loop with alive function

--- hero_rpg.py
+++ hero_rpg.py
@@ -66,7 +66,7 @@ def main():
     # goblin_health = 6
     # goblin_power = 2
 
-    while goblin.health > 0 and hero.health > 0:
+    while goblin.alive() > 0 and hero.alive() > 0:
         print("You have {} health and {} power.".format(hero.health, hero.power))
         print("The goblin has {} health and {} power.".format(
             goblin.health, goblin.power))
@@ -79,11 +79,6 @@ def main():
         raw_input = input()
         if raw_input == "1":
             hero.attack(goblin)
-            # Hero attacks goblin
-            # goblin.health -= hero.power
-            # print("You do {} damage to the goblin.".format(hero.power))
-            # if goblin.health <= 0:
-            #     print("The goblin is dead.")
         elif raw_input == "2":
             pass
         elif raw_input == "3":
@@ -94,11 +89,6 @@ def main():
 
         if goblin.health > 0:
             goblin.attack(hero)
-            # Goblin attacks hero
-            # hero.health -= goblin.power
-            # print("The goblin does {} damage to you.".format(goblin.power))
-            # if hero.health <= 0:
-            #     print("You are dead.")
 
 
 main()
